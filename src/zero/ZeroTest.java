5b65a0d2a0d54c22cb1ed73dba4a7f97542a7637
wangdongdong <827495316@qq.com>
2019-09-03 00:04:09 +0800

    zero-test

---
+++ src/zero/ZeroTest.java
@@ -0,0 +1,13 @@
+package zero;
+
+/**
+ * @program: java-algorithm
+ * @description: ${description}
+ * @author: Mr.Wang
+ * @create: 2019-09-03 00:03
+ **/
+public class ZeroTest {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
